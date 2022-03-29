69ba20bb20bcffd73f68f15b941403dca6eca721
Gary <gblake@vmware.com>
2022-03-29 11:44:26 +0100

    Creating Initial Base Files from Prototype

---
+++ VcfUpgradePrecheck.psm1
@@ -0,0 +1,243 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+### Note
+# This PowerShell module should be considered entirely experimental. It is still in development & not tested beyond lab
+# scenarios. It is recommended you don't use it for any production environment without testing extensively!
+
+# Enable communication with self signed certs when using Powershell Core. If you require all communications to be secure
+# and do not wish to allow communication with self-signed certificates remove lines 13-36 before importing the module.
+
+if ($PSEdition -eq 'Core') {
+    $PSDefaultParameterValues.Add("Invoke-RestMethod:SkipCertificateCheck", $true)
+}
+
+if ($PSEdition -eq 'Desktop') {
+    # Enable communication with self signed certs when using Windows Powershell
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+    if ("TrustAllCertificatePolicy" -as [type]) {} else {
+        Add-Type @"
+	using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertificatePolicy : ICertificatePolicy {
+        public TrustAllCertificatePolicy() {}
+		public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate certificate,
+            WebRequest wRequest, int certificateProblem) {
+            return true;
+        }
+	}
+"@
+        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertificatePolicy
+    }
+}
+
+Function Invoke-SddcCommand {
+    <#
+		.SYNOPSIS
+        Execute a command on SDDC Manager
+
+        .DESCRIPTION
+        The Invoke-SddcCommand cmdlets executes a command within the SDDC Manager appliance. The cmdlet connects to SDDC
+        Manager using the -server, -user, and -password values:
+        - Validates that network connectivity is available to the SDDC Manager instance
+        - Validates that network connectivity is available to the Management Domain vCenter Server instance
+        - Executes the command provided within the SDDC Manager appliance
+
+        .EXAMPLE
+        Invoke-SddcCommand -server sfo-vcf01.sfo.rainpole.io -user administrator@vsphere.local -pass VMw@re1! -rootPass VMw@re1! -command "chage -l backup"
+        This example executes the command provided on the SDDC Manager appliance
+    #>
+
+    Param (
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$server,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$user,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$pass,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$rootPass,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$command
+    )
+
+    if (Test-VCFConnection -server $server) {
+        if (Test-VCFAuthentication -server $server -user $user -pass $pass) {
+            if (($vcfVcenterDetails = Get-vCenterServerDetail -server $server -user $user -pass $pass -domainType MANAGEMENT)) {
+                if (Test-VsphereConnection -server $($vcfVcenterDetails.fqdn)) {
+                    if (Test-VsphereAuthentication -server $vcfVcenterDetails.fqdn -user $vcfVcenterDetails.ssoAdmin -pass $vcfVcenterDetails.ssoAdminPass) {
+                        $output = Invoke-VMScript -VM ($server.Split(".")[0]) -ScriptText $command -GuestUser root -GuestPassword $rootPass -Server $vcfVcenterDetails.fqdn
+                        $output
+                    }
+                }
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Invoke-SddcCommand
+
+Function Export-SystemPassword {
+    <#
+		.SYNOPSIS
+        Generates a system password report 
+
+        .DESCRIPTION
+        The Export-SystemPassword cmdlets generates a system password report from SDDC Manager. The cmdlet connects to
+        SDDC Manager using the -server, -user, and -password values:
+        - Validates that network connectivity is available to the SDDC Manager instance
+        - Generates a system password report from SDDC Manager and outputs to an HTML format
+
+        .EXAMPLE
+        Export-SystemPassword -server sfo-vcf01.sfo.rainpole.io -user administrator@vsphere.local -pass VMw@re1!
+        Export-SystemPassword -server ldn-vcf01.ldn.cloudy.io -user administrator@vsphere.local -pass VMw@re1!
+        This example generates a system password report from SDDC Manager
+    #>
+
+    Param (
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$server,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$user,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$pass,
+        [Parameter (Mandatory = $false)] [ValidateNotNullOrEmpty()] [Switch]$html
+    )
+
+    if (Test-VCFConnection -server $server) {
+        if (Test-VCFAuthentication -server $server -user $user -pass $pass) {
+            if ($PsBoundParameters.ContainsKey("html")) {
+                Get-VCFCredential | Select-Object @{Name="Workload Domain"; Expression={ $_.resource.domainName}}, @{Name="FQDN"; Expression={ $_.resource.resourceName}}, @{Name="IP Address"; Expression={ $_.resource.resourceIp}}, accountType, username, password | Where-Object {$_.accountType -eq "USER" -or $_.accountType -eq "SYSTEM"} | Sort-Object "Domain Name", "FQDN" | ConvertTo-Html -Fragment -PreContent "<h2>System Passwords from SDDC Manager</h2>" -As Table
+            }
+            else {
+                Get-VCFCredential | Select-Object @{Name="Workload Domain"; Expression={ $_.resource.domainName}}, @{Name="FQDN"; Expression={ $_.resource.resourceName}}, @{Name="IP Address"; Expression={ $_.resource.resourceIp}}, accountType, username, password | Where-Object {$_.accountType -eq "USER" -or $_.accountType -eq "SYSTEM"} | Sort-Object "Domain Name", "FQDN"
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Export-SystemPassword
+
+Function Export-EsxiCoreDumpConfig {
+    <#
+		.SYNOPSIS
+        Generates a storage capacity report
+
+        .DESCRIPTION
+        The Export-EsxiCoreDumpConfig cmdlets generates a storage capacity report for a Workload Domain. The cmdlet
+        connects to SDDC Manager using the -server, -user, and -password values:
+        - Validates that network connectivity is available to the SDDC Manager instance
+        - Validates that network connectivity is available to the vCenter Server instance
+        - Generates a storage capacity report for all clusters of the Workload Domain
+
+        .EXAMPLE
+        Export-EsxiCoreDumpConfig -server sfo-vcf01.sfo.rainpole.io -user administrator@vsphere.local -pass VMw@re1! -sddcDomain sfo-m01
+        Export-EsxiCoreDumpConfig -server ldn-vcf01.ldn.cloudy.io -user administrator@vsphere.local -pass VMw@re1! -sddcDomain ldn-m01
+        This example generates a storage capacity report for the Workload Domain named 'sfo-m01'
+    #>
+
+    Param (
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$server,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$user,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$pass,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcDomain,
+        [Parameter (Mandatory = $false)] [ValidateNotNullOrEmpty()] [Switch]$html
+    )
+
+    if (Test-VCFConnection -server $server) {
+        if (Test-VCFAuthentication -server $server -user $user -pass $pass) {
+            if (($vcfVcenterDetails = Get-vCenterServerDetail -server $server -user $user -pass $pass -domain $sddcDomain)) {
+                if (Test-VsphereConnection -server $($vcfVcenterDetails.fqdn)) {
+                    if (Test-VsphereAuthentication -server $vcfVcenterDetails.fqdn -user $vcfVcenterDetails.ssoAdmin -pass $vcfVcenterDetails.ssoAdminPass) {
+                        $coreDumpObject = New-Object -TypeName psobject
+                        $allHostObject = New-Object System.Collections.ArrayList
+                        $esxiHosts = Get-VMHost 
+                        Foreach ($esxiHost in $esxiHosts) {
+                            $coreDumpObject = New-Object -TypeName psobject
+                            $esxcli = Get-EsxCli -VMhost $esxiHost.Name -V2
+                            $coreDumpConfig = $esxcli.system.coredump.partition.get.invoke()
+                            $coreDumpObject | Add-Member -notepropertyname 'Host' -notepropertyvalue $esxiHost.Name
+                            $coreDumpObject | Add-Member -notepropertyname 'Active Core Dump' -notepropertyvalue $coreDumpConfig.Active
+                            $coreDumpObject | Add-Member -notepropertyname 'Configured Core Dump' -notepropertyvalue $coreDumpConfig.Configured
+                            $allHostObject += $coreDumpObject
+                        }
+                        if ($PsBoundParameters.ContainsKey("html")) {
+                            $allHostObject | ConvertTo-Html -Fragment -PreContent "<h3>ESXi Core Dump Configurtion for Workload Domain $sddcDomain</h3>" -As Table
+                        }
+                        else {
+                            $allHostObject
+                        }
+                    }
+                    Disconnect-VIServer * -Force -Confirm:$false -WarningAction SilentlyContinue | Out-Null
+                }
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Export-EsxiCoreDumpConfig
+
+Function Export-StorageCapacity {
+    <#
+		.SYNOPSIS
+        Generates a storage capacity report
+
+        .DESCRIPTION
+        The Export-StorageCapacity cmdlets generates a storage capacity report for a Workload Domain. The cmdlet
+        connects to SDDC Manager using the -server, -user, and -password values:
+        - Validates that network connectivity is available to the SDDC Manager instance
+        - Validates that network connectivity is available to the vCenter Server instance
+        - Generates a storage capacity report for all clusters of the Workload Domain
+
+        .EXAMPLE
+        Export-StorageCapacity -server sfo-vcf01.sfo.rainpole.io -user administrator@vsphere.local -pass VMw@re1! -sddcDomain sfo-m01
+        Export-StorageCapacity -server ldn-vcf01.ldn.cloudy.io -user administrator@vsphere.local -pass VMw@re1! -sddcDomain ldn-m01
+        This example generates a storage capacity report for the Workload Domain named 'sfo-m01'
+    #>
+
+    Param (
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$server,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$user,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$pass,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcDomain,
+        [Parameter (Mandatory = $false)] [ValidateNotNullOrEmpty()] [Switch]$html
+    )
+
+    if (Test-VCFConnection -server $server) {
+        if (Test-VCFAuthentication -server $server -user $user -pass $pass) {
+            if (($vcfVcenterDetails = Get-vCenterServerDetail -server $server -user $user -pass $pass -domain $sddcDomain)) {
+                if (Test-VsphereConnection -server $($vcfVcenterDetails.fqdn)) {
+                    if (Test-VsphereAuthentication -server $vcfVcenterDetails.fqdn -user $vcfVcenterDetails.ssoAdmin -pass $vcfVcenterDetails.ssoAdminPass) {
+                        $datastores = Get-Datastore | Sort-Object Name
+                        Foreach ($datastore in $datastores) {
+                            if (($datastore.Name -match "Shared") -or ($datastore.Name -match "")) {
+                                $PercentFree = PercentCalc $datastore.FreeSpaceMB $datastore.CapacityMB
+                                $PercentFree = "{0:N2}" -f $PercentFree
+                                $datastore | Add-Member -type NoteProperty -name PercentFree -value $PercentFree
+                            }
+                        }
+                        if ($PsBoundParameters.ContainsKey("html")) {
+                            $datastores | Select-Object Name,@{N="Used Space GB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace)/1GB,0)}},@{N="Total Space GB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity)/1GB,0)}} ,PercentFree | ConvertTo-Html -Fragment -PreContent "<h3>Datastore Storage Capacity for Workload Domain $sddcDomain</h3>" -As Table
+                        }
+                        else {
+                            $datastores | Select-Object Name,@{N="Used Space GB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity - $_.ExtensionData.Summary.FreeSpace)/1GB,0)}},@{N="Total Space GB";E={[Math]::Round(($_.ExtensionData.Summary.Capacity)/1GB,0)}} ,PercentFree
+                        }
+                    }
+                    Disconnect-VIServer * -Force -Confirm:$false -WarningAction SilentlyContinue | Out-Null
+                }
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Export-StorageCapacity
+
+
+Function Convert-TextToHtml {
+    Param (
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sourceFile,
+        [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$label
+    )
+
+    Get-Content $sourceFile | ConvertTo-HTML -Property @{Label=$label;Expression={$_}} -Fragment
+}
+Export-ModuleMember -Function Convert-TextToHtml
+
+Function PercentCalc {
+    Param (
+        [Parameter (Mandatory = $true)] [Int]$InputNum1,
+        [Parameter (Mandatory = $true)] [Int]$InputNum2)
+        $InputNum1 / $InputNum2*100
+}
---
+++ precheck.ps1
@@ -0,0 +1,135 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+<#
+    .NOTES
+    ===================================================================================================================
+    Created by:  Gary Blake - Senior Staff Solutions Architect
+    Date:   2022-03-29
+    Copyright 2021-2022 VMware, Inc.
+    ===================================================================================================================
+    .CHANGE_LOG
+
+    - 1.0.000   (Gary Blake / 2022-03-17) - Initial script creation
+
+    ===================================================================================================================
+    
+    .SYNOPSIS
+    Perform health checks across and SDDC Manager instance
+
+    .DESCRIPTION
+    The precheck.ps1 provides a single script to perform health checks across an SDDC Manager instance
+
+    .EXAMPLE
+    precheck.ps1 -sddcManagerFqdn sfo-vcf01.sfo.rainpole.io -sddcManagerUser administrator@vsphere.local -sddcManagerPass VMw@re1! -sddcManagerRootPass VMw@re1!
+    This example performs multiple system prechecks for an SDDC Manager instance
+#>
+
+Param (
+    [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcManagerFqdn,
+    [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcManagerUser,
+    [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcManagerPass,
+    [Parameter (Mandatory = $true)] [ValidateNotNullOrEmpty()] [String]$sddcManagerRootPass
+)
+
+Clear-Host; Write-Host ""
+
+Start-SetupLogFile -Path $filePath -ScriptName $MyInvocation.MyCommand.Name
+Write-LogMessage -Type INFO -Message "Starting the Process of Running Health Checks for VMware Cloud Foundation Instance ($sddcManagerFqdn)" -Colour Yellow
+Write-LogMessage -Type INFO -Message "Setting up the log file to path $logfile"
+
+
+# Setup the file name of the HTML based health check report
+$filetimeStamp = Get-Date -Format "MM-dd-yyyy_hh_mm_ss"
+$reportLocation = ".\"
+$reportFolder = "reports"
+$reportsPath = $reportLocation + $reportFolder
+if (!(Test-Path -Path $reportsPath)) {
+    New-Item -Path $reportLocation -Name $reportFolder -ItemType "directory" | Out-Null
+}
+$reportName = $reportsPath + "\" + $sddcManagerFqdn.Split(".")[0] + "-healthCheck-" + $filetimeStamp + ".htm"
+
+# Define the formatting of the HTML report using CSS
+$reportFormat = @"
+<style>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #1A4288;
+        font-size: 30px;
+    }
+    h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #459B36;
+        font-size: 20px;
+    }
+    h3 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #7F35B2;
+        font-size: 16px;
+    }
+    table {
+		font-size: 12px;
+		border: 0px; 
+		font-family: monospace;
+	} 
+    td {
+		padding: 4px;
+		margin: 0px;
+		border: 0;
+	}
+    th {
+        background: #717074;
+        background: linear-gradient(#464547, #717074);
+        color: #fff;
+        font-size: 11px;
+        text-transform: capitalize;
+        padding: 10px 15px;
+        vertical-align: middle;
+	}
+    tbody tr:nth-child(even) {
+        background: #f0f0f2;
+    }
+        #CreationDate {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #ff3300;
+        font-size: 12px;
+    }
+</style>
+"@
+
+# Define the Report Tile
+$reportTitle = "<h1>Health Check Report for SDDC Manager: $sddcManagerFqdn</h1>"
+
+# Generating the System Password Report from SDDC Manager 
+Write-LogMessage -Type INFO -Message "Generating the System Password Report from SDDC Manager ($sddcManagerFqdn)"
+$systemPasswordHtml = Export-SystemPassword -server $sddcManagerFqdn -user $sddcManagerUser -pass $sddcManagerPass -html
+
+$datastoreTitle = "<h2>Datastore Capacity for all Workload Domains</h2>"
+# Generating Datastore Capacity Report for all Workload Domains
+Write-LogMessage -Type INFO -Message "Generating Datastore Capacity Report for all Workload Domains"
+$allWorkloadDomain = Get-VCFWorkloadDomain | Select-Object name
+foreach ($workloadDomain in $allWorkloadDomain) {   
+    $storageCapacityHtml = Export-StorageCapacity -server $sddcManagerFqdn -user $sddcManagerUser -pass $sddcManagerPass -sddcDomain $workloadDomain.name -html
+    $allStorageCapacityHtml += $storageCapacityHtml
+}
+
+$coreDumpTitle = "<h2>ESXi Host Core Dump Configuration for all Workload Domains</h2>"
+# Generating ESXi Host Core Dump Configuaration for all Workload Domains
+Write-LogMessage -Type INFO -Message "Generating ESXi Host Core Dump Configuaration for all Workload Domains"
+$allWorkloadDomain = Get-VCFWorkloadDomain | Select-Object name
+foreach ($workloadDomain in $allWorkloadDomain) {
+    Write-LogMessage -Type INFO -Message "Gathering ESXi Host Core Dump Configuaration for Workload Domain ($($workloadDomain.name))"
+    $esxiCoreDumpHtml = Export-EsxiCoreDumpConfig -server $sddcManagerFqdn -user $sddcManagerUser -pass $sddcManagerPass -sddcDomain $workloadDomain.name -html
+    $allEsxiCoreDumpHtml += $esxiCoreDumpHtml
+}
+
+# Combine all information gathered into a single HTML report
+$report = ConvertTo-HTML -Body "$reportTitle $systemPasswordHtml $datastoreTitle $allStorageCapacityHtml $coreDumpTitle $allEsxiCoreDumpHtml " -Title "SDDC Manager Health Check Report" -Head $reportFormat -PostContent "<p>Creation Date: $(Get-Date)<p>"
+
+# Generate the report to an HTML file and then open it in the default browser
+Write-LogMessage -Type INFO -Message "Generating the Final Report and Saving to ($reportName)"
+$report | Out-File $reportName
+Invoke-Item $reportName
+
