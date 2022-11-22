3c64adcff450f0730a6d1b67b2c3dff2dcb59058
Ryan Johnson <johnsonryan@vmware.com>
2022-11-22 13:31:14 -0500

    fix: update prerequisites version check
    Updates `Test-VcfReportingPrereq` to display the current installed module version versus the minimum version in the module requirements array.
    
    Closes #26
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- VMware.CloudFoundation.Reporting.psm1
+++ VMware.CloudFoundation.Reporting.psm1
@@ -8453,7 +8453,8 @@ Function Test-VcfReportingPrereq {
                     Write-Warning $message; Write-Host ""
 					Exit
                 } else {
-                    $message = "PowerShell Module: $($module.Name) Version: $($module.Version) Found, Supports the minimum required version."
+                    $moduleCurrentVersion = (Get-InstalledModule -Name $module.Name).Version
+                    $message = "PowerShell Module: $($module.Name) Version: $($moduleCurrentVersion) Found, Supports the minimum required version."
                     $message
                 }
             } else {
@@ -8463,7 +8464,8 @@ Function Test-VcfReportingPrereq {
                         Write-Warning $message; Write-Host ""
 					Exit
                     } else {
-                        $message = "PowerShell Module: $($module.Name) Version: $($module.Version) Found, Supports the minimum required version."
+                        $moduleCurrentVersion = (Get-InstalledModule -Name $module.Name).Version
+                        $message = "PowerShell Module: $($module.Name) Version: $($moduleCurrentVersion) Found, Supports the minimum required version."
                         $message
                     }
                 }
