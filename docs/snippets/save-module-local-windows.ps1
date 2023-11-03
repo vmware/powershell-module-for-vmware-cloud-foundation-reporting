cbc0caa1e58c9b75bc6200ccbad69eb6275d8015
Jared Burns <burnsja@vmware.com>
2023-11-03 07:25:45 -0400

    :docs/air-gapped
    Documentation has been updated with air-gapped information.
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

---
+++ docs/snippets/save-module-local-windows.ps1
@@ -0,0 +1,5 @@
+Save-Module -Name VMware.PowerCLI -Path F:\Module\
+Save-Module -Name VMware.vSphere.SsoAdmin -Path F:\Module\
+Save-Module -Name PowerVCF -Path F:\Module\
+Save-Module -Name PowerValidatedSolutions -Path F:\Module\
+Save-Module -Name VMware.CloudFoundation.Reporting -Path F:\Module\
