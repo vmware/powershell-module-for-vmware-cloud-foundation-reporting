0899c29717801b8de9ed32221a91414b670c5ab7
Jared Burns <burnsja@vmware.com>
2023-11-16 09:03:29 -0500

    docs: update install module with repository flag (#189)
    Update the installation documentation to specify the PowerShell Gallery as the source repository.
    ---------
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>
    Co-authored-by: Ryan Johnson <ryan@tenthirtyam.org>

--- docs/snippets/save-module-local-windows.ps1
+++ docs/snippets/save-module-local-windows.ps1
@@ -1,5 +1,5 @@
-Save-Module -Name VMware.PowerCLI -Path F:\Module\
-Save-Module -Name VMware.vSphere.SsoAdmin -Path F:\Module\
-Save-Module -Name PowerVCF -Path F:\Module\
-Save-Module -Name PowerValidatedSolutions -Path F:\Module\
-Save-Module -Name VMware.CloudFoundation.Reporting -Path F:\Module\
+Save-Module -Name VMware.PowerCLI -Path F:\Module\ -Repository PSGallery
+Save-Module -Name VMware.vSphere.SsoAdmin -Path F:\Module\ -Repository PSGallery
+Save-Module -Name PowerVCF -Path F:\Module\ -Repository PSGallery
+Save-Module -Name PowerValidatedSolutions -Path F:\Module\ -Repository PSGallery
+Save-Module -Name VMware.CloudFoundation.Reporting -Path F:\Module\ -Repository PSGallery
